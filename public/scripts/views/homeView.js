2ab486891e0e9b6c358c39983fe79e083b52170b
Kelati Andemichael <Kelati@Macbooks-MacBook-Pro-2.local>
2017-09-09 13:50:03 -0700

    sports selection dropdown 06

--- public/scripts/views/homeView.js
+++ public/scripts/views/homeView.js
@@ -4,5 +4,15 @@ var app = app || {};
 
 (function(module) {
 
+  $('.dropdown').click(function( event ) {
+    event.preventDefault();
+    var sport = $( 'select#dropdown').val();
+    // val is select, it hide it, if not show it
+    if (sport == '#draft-configure')
+      $('#draft-configure').show(IDK);
+    else
+      $('#draft-configure').hide(IDK);
+  });
+
   module.homeView = homeView;
 })(app);
