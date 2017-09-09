6996456ff785d54cccad5ea0229f241aeaff79a2
Larry Cessna <larry.cessna@outlook.com>
2017-09-09 15:55:18 -0700

    Issue 15, Randomize button works.

--- public/scripts/views/historyView.js
+++ public/scripts/views/historyView.js
@@ -3,6 +3,7 @@
 var app = app || {};
 
 (function(module) {
+  let historyView = [];
 
   module.historyView = historyView;
 })(app);
--- public/scripts/views/homeView.js
+++ public/scripts/views/homeView.js
@@ -3,16 +3,25 @@
 var app = app || {};
 
 (function(module) {
+let homeView = [];
 
   $('.dropdown').change(function( event ) {
     event.preventDefault();
     // val is select, it hide it, if not show it
-    
+
     if ($( 'select#dropdown').val() === "select-sport") {
-      $('.draft-config').hide();  
+      $('.draft-config').hide();
     } else {
       $('.draft-config').show();
     }
+  });
+
+  $('#random-btn').click(function ( event ){
+    event.preventDefault();
+        var options = $('#userPos').children('option');
+        var random = Math.floor(options.length * (Math.random() % 1));
+        options.attr('selected', false).eq(random).attr('selected', true);
+        console.log(options);
   });
 
   module.homeView = homeView;
