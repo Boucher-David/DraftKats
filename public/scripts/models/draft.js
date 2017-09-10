062c7da21dea6f7c5799bcaf2057dbc43ef1e752
hanhthaoluu <hanhthaoluu@gmail.com>
2017-09-09 17:28:56 -0700

    displayed content-draft in draft.js

--- public/scripts/models/draft.js
+++ public/scripts/models/draft.js
@@ -3,6 +3,14 @@
 var app = app || {};
 
 (function(module) {
+  let populateDraft = function() {
+    var source   = $("#draft-template").html();
+    var template = Handlebars.compile(source);
 
-  
+    for (var i = 0; i < 10; i++) {
+    $('#content-draft').append(template);
+    }
+  };
+  populateDraft();
+
 })(app);
